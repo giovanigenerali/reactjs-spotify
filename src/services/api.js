34e2644993912da381c41b30be16d629123031f5
Giovani Generali <giovani.generali@gmail.com>
2019-03-26 15:39:08 -0300

    fix dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SPOTIFY_API || 'https://localhost:3001',
+  baseURL: process.env.HOST_API || process.env.REACT_APP_HOST_API || 'http://localhost:3001',
 });
 
 export default api;
